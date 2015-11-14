e52211e143b71f9e34638a48d5a48baf515b79f7
dingjun <dingjun0225@gmail.com>
2015-11-14 11:42:05 +0800

    close debug

--- GreateWorld/src/com/org/great/world/Utils/Debug.java
+++ GreateWorld/src/com/org/great/world/Utils/Debug.java
@@ -8,7 +8,7 @@ import android.util.Log;
  */
 public class Debug {
     private static String TAG = "JIWAI";
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
 
     public static void d(String tag, String msg) {
         if (DEBUG) {
